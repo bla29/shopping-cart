d73b54ebf3613caa1eae26c20be510dd03f3acf9
Brian La <133820463+bla29@users.noreply.github.com>
2026-04-13 17:05:48 -0700

    add to cart button working

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -36,34 +36,38 @@ function Products({ cartProducts, setCartProducts }) {
         return catalogProducts.findIndex(product => product.id === key);
     }
 
-    function productCartCount(key) {
+    function inputProductCount(key) {
         let counter = 0;
-        for (let i = 0; i < cartProducts.length; i++) {
-            if (cartProducts[i].id == key) {
+        for (let i = 0; i < inputProducts.length; i++) {
+            if (inputProducts[i].id == key) {
                 counter++;
             }
         }
         return counter;
     }
 
-    function findProductCart(key) {
-        return cartProducts.findIndex(product => product.id === key);
+    function findInputProduct(key) {
+        return inputProducts.findIndex(product => product.id === key);
     }
 
     function addProduct(key) {
         if (findProductCatalog(key) != -1) {
             let newProduct = catalogProducts[findProductCatalog(key)];
-            setCartProducts(prev => [...prev, newProduct]);
+            setInputProducts(prev => [...prev, newProduct]);
         }
     }
 
     function deleteProduct(key) {
-        if (findProductCart(key) != -1) {
-            let newCartArr = [...cartProducts];
-            let deleteIndex = findProductCart(key);
-            newCartArr.splice(deleteIndex, 1);
-            setCartProducts(newCartArr);
+        if (findInputProduct(key) != -1) {
+            let newInputArr = [...inputProducts];
+            let deleteIndex = findInputProduct(key);
+            newInputArr.splice(deleteIndex, 1);
+            setInputProducts(newInputArr);
         }
+    }
+
+    function setCart() {
+        setCartProducts(inputProducts);
     }
 
     function productsList() {
@@ -74,11 +78,11 @@ function Products({ cartProducts, setCartProducts }) {
                     <h3 className="item-text">{product.title}</h3>
                     <h3 className="item-text">${product.price}</h3>
                     <div>
-                        <input type="number" name="input1" value={productCartCount(product.id)}></input>
+                        <input type="number" name="input1" value={inputProductCount(product.id)}></input>
                         <button onClick={() => deleteProduct(product.id)}>-</button>
                         <button onClick={() => addProduct(product.id)}>+</button>
                     </div>
-                    <button>Add to Cart</button>
+                    <button onClick={() => setCart()}>Add to Cart</button>
                 </div>
             )
         })
